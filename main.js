c52ef0ec1b1a0690c2dddc7f627648ecb82e298a
Enbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2025-11-10 23:23:55 +0000

    Add options box

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Constants
 const fileInput = document.getElementById("tracks_upload");
-const uploadButton = document.getElementById("add_tracks")
+const uploadButton = document.getElementById("add_tracks");
 const trackList = document.getElementById("track_select");
 
-//Defining a listener for our button, specifically, an onclick handler
-uploadButton.onclick = function() {
+const collapsibleToggles = document.getElementsByClassName("collapsible_toggle");
 
+uploadButton.onclick = function() {
     // Add all files to trackList
     for (const file of fileInput.files) {
         var option = document.createElement("option");
@@ -20,3 +20,17 @@ uploadButton.onclick = function() {
 trackList.addEventListener('change', function (e) {
     alert('changed');
 });
+
+for (const toggle of collapsibleToggles) {
+    toggle.onclick = function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+            this.textContent = "Show";
+        } else {
+            content.style.display = "block";
+            this.textContent = "Hide";
+        }
+    }
+}
